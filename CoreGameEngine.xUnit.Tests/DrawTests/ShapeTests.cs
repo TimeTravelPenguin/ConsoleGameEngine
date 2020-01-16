721c99b79bcd253086b161533bed3be5123d7f01
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-17 02:08:56 +1100

    Created

---
+++ CoreGameEngine.xUnit.Tests/DrawTests/ShapeTests.cs
@@ -0,0 +1,22 @@
+﻿using CoreGameEngine.Draw;
+using FluentAssertions;
+using Xunit;
+
+namespace CoreGameEngine.xUnit.Tests.DrawTests
+{
+  public class ShapeTests
+  {
+    [Theory]
+    [InlineData(@"u2 r 2 ", new[] {"u2", "r 2"})]
+    [InlineData(@"c red u2 r 2 ", new[] {"c red", "u2", "r 2"})]
+    [InlineData(@"c red n1 2 u2 r 2 ", new[] {"c red", "n1 2", "u2", "r 2"})]
+    [InlineData(@"n1 2 c red u2 r 2 ", new[] {"n1 2", "c red", "u2", "r 2"})]
+    public void ShapeValidationTests(string regex, string[] expected)
+    {
+      var actual = Shape.New(regex);
+
+      actual.Should()
+        .BeEquivalentTo(expected);
+    }
+  }
+}
---
+++ CoreGameEngine/Extensions/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using CoreGameEngine.Resources;
+
+namespace CoreGameEngine.Extensions
+{
+  public static class StringExtensions
+  {
+    public static bool StartsWith(this string input, IEnumerable<string> values)
+    {
+      if (values is null)
+      {
+        throw new ArgumentNullException(nameof(values), Exceptions.ArgumentIsNull);
+      }
+
+      return values.Any(value => input.StartsWith(value, StringComparison.InvariantCulture));
+    }
+
+    public static string FirstCharToUpper(this string input)
+    {
+      return input switch
+      {
+        null => throw new ArgumentNullException(nameof(input)),
+        "" => throw new ArgumentException(
+          string.Format(CultureInfo.InvariantCulture, Exceptions.Argument_StringEmpty, nameof(input)), nameof(input)),
+        _ => input.First().ToString(CultureInfo.InvariantCulture).ToUpperInvariant() + input.Substring(1)
+      };
+    }
+  }
+}
