aec09e6dafd2048e84f6bbd59896ff4899694a7f
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-18 01:54:02 +1100

    Created

---
+++ CoreGameEngine/Extensions/ArrayExtensions.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Linq;
+using CoreGameEngine.Resources;
+
+namespace CoreGameEngine.Extensions
+{
+  public enum Rotation
+  {
+    Clockwise,
+    CounterClockwise
+  }
+
+  public static class ArrayExtensions
+  {
+    public static T[,] Transpose<T>(this T[,] matrix)
+    {
+      if (matrix is null)
+      {
+        throw new ArgumentNullException(nameof(matrix), Exceptions.Argument_IsNull);
+      }
+
+      var w = matrix.GetLength(0);
+      var h = matrix.GetLength(1);
+
+      var result = new T[h, w];
+
+      for (var i = 0; i < w; i++)
+      {
+        for (var j = 0; j < h; j++)
+        {
+          result[j, i] = matrix[i, j];
+        }
+      }
+
+      return result;
+    }
+
+    public static T[,] RotateMatrix<T>(this T[,] matrix, Rotation rotation)
+    {
+      if (matrix is null)
+      {
+        throw new ArgumentNullException(nameof(matrix), Exceptions.Argument_IsNull);
+      }
+
+      var transpose = matrix.Transpose();
+
+      var tRow = transpose.GetLength(0);
+      var tCol = transpose.GetLength(1);
+
+      var newArr = new T[tRow, tCol];
+
+      switch (rotation)
+      {
+        case Rotation.Clockwise:
+        {
+          for (var y = 0; y < tRow; y++)
+          {
+            for (var x = 0; x < tCol; x++)
+            {
+              newArr[y, x] = transpose[y, tCol - x - 1];
+            }
+          }
+
+          break;
+        }
+        case Rotation.CounterClockwise:
+        {
+          for (var y = 0; y < tRow; y++)
+          {
+            for (var x = 0; x < tCol; x++)
+            {
+              newArr[y, x] = transpose[tRow - y - 1, x];
+            }
+          }
+
+          break;
+        }
+        default:
+          throw new ArgumentOutOfRangeException(nameof(rotation), rotation, null);
+      }
+
+      return newArr;
+    }
+
+    public static T[][] RotateMatrix<T>(this T[][] jaggadArray, Rotation rotation)
+    {
+      var arr2D = jaggadArray.To2DArray();
+      var rotated2D = arr2D.RotateMatrix(rotation);
+      return rotated2D.ToJaggedArray();
+    }
+
+    public static T[,] To2DArray<T>(this T[][] jaggadArray)
+    {
+      if (jaggadArray is null)
+      {
+        throw new ArgumentNullException(nameof(jaggadArray), Exceptions.Argument_IsNull);
+      }
+
+      if (!jaggadArray.IsRectangular())
+      {
+        throw new InvalidOperationException(Exceptions.Array_JaggadNotRectangular);
+      }
+
+      var columns = jaggadArray.Length;
+
+      // throws InvalidOperationException if jaggadArray is not rectangular
+      var row = jaggadArray.GroupBy(r => r.Length)
+        .Single()
+        .Key;
+
+      var result = new T[columns, row];
+      for (var i = 0; i < columns; ++i)
+      {
+        for (var j = 0; j < row; ++j)
+        {
+          result[i, j] = jaggadArray[i][j];
+        }
+      }
+
+      return result;
+    }
+
+    public static T[][] ToJaggedArray<T>(this T[,] array)
+    {
+      var rowsFirstIndex = array.GetLowerBound(0);
+      var rowsLastIndex = array.GetUpperBound(0);
+      var numberOfRows = rowsLastIndex - rowsFirstIndex + 1;
+
+      var columnsFirstIndex = array.GetLowerBound(1);
+      var columnsLastIndex = array.GetUpperBound(1);
+      var numberOfColumns = columnsLastIndex - columnsFirstIndex + 1;
+
+      var jaggedArray = new T[numberOfRows][];
+      for (var i = 0; i < numberOfRows; i++)
+      {
+        jaggedArray[i] = new T[numberOfColumns];
+
+        for (var j = 0; j < numberOfColumns; j++)
+        {
+          jaggedArray[i][j] = array[i + rowsFirstIndex, j + columnsFirstIndex];
+        }
+      }
+
+      return jaggedArray;
+    }
+
+    public static bool IsRectangular<T>(this T[][] matrix)
+    {
+      return matrix.All(x => x.Length == matrix.GetLength(0));
+    }
+  }
+}
