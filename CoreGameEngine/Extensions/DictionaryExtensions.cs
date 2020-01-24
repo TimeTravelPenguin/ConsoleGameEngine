ed95774b3f2dc781e714875a6a75b7694991d2f0
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-24 18:54:28 +1100

    Removed old code

--- CoreGameEngine/Extensions/DictionaryExtensions.cs
+++ CoreGameEngine/Extensions/DictionaryExtensions.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Drawing;
-using System.Linq;
 using CoreGameEngine.Resources;
 
 namespace CoreGameEngine.Extensions
@@ -26,40 +23,6 @@ namespace CoreGameEngine.Extensions
       {
         throw new InvalidOperationException(Exceptions.Dictionary_InvalidKey);
       }
-    }
-
-    public static (Point, T)[][] PointDictionaryToArray<T>([NotNull] this IDictionary<Point, T> dictionary)
-      where T : class
-    {
-      if (dictionary is null)
-      {
-        throw new ArgumentNullException(nameof(dictionary), Exceptions.ArgumentIsNull);
-      }
-
-      if (dictionary.Values.Contains(null))
-      {
-        throw new NullReferenceException(Exceptions.Dictionary_CannotContainNullValues);
-      }
-
-      var minX = dictionary.Keys.Select(point => point.X).Min();
-      var maxX = dictionary.Keys.Select(point => point.X).Max();
-      var minY = dictionary.Keys.Select(point => point.Y).Min();
-      var maxY = dictionary.Keys.Select(point => point.Y).Max();
-
-      var matrixArr = new (Point, T)[maxY - minY + 1][];
-
-      for (var y = minY; y <= maxY; y++)
-      {
-        for (var x = minX; x <= maxX; x++)
-        {
-          var p = new Point(x, y);
-          var value = dictionary.ContainsKey(p) ? dictionary[p] : null;
-
-          matrixArr[y][x] = (p, value);
-        }
-      }
-
-      return matrixArr;
     }
   }
 }
