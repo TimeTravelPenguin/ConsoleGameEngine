ba4a7e39d2d314566c954da3e4365f6805610730
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-24 18:55:51 +1100

    Create new shape extension method

--- CoreGameEngine/Extensions/ShapeExtensions.cs
+++ CoreGameEngine/Extensions/ShapeExtensions.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using CoreGameEngine.Resources;
+using CoreGameEngine.Shapes;
 
 namespace CoreGameEngine.Extensions
 {
   public static class ShapeExtensions
   {
-    
+    public static IShape CreateNewShapeFrom<TType>(this IShape shape)
+      where TType : IShape
+    {
+      return shape.CreateNewShapeFrom(typeof(TType));
+    }
+
+    public static IShape CreateNewShapeFrom(this IShape shape, Type toType)
+    {
+      if (shape is null)
+      {
+        throw new ArgumentNullException(nameof(shape), Exceptions.ArgumentIsNull);
+      }
+
+      if (toType is null)
+      {
+        throw new ArgumentNullException(nameof(toType), Exceptions.ArgumentIsNull);
+      }
+
+      if (!typeof(IShape).IsAssignableFrom(toType))
+      {
+        throw new InvalidOperationException();
+      }
+
+      var newShape = (IShape) Activator.CreateInstance(toType);
+      newShape.SetGlyphs(new Dictionary<Point, Glyph>(shape.Glyphs));
+
+      return newShape;
+    }
   }
 }
