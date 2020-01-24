d7c62a75cd1bdc22c0d2d6af30bae62112aaaa49
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-24 18:56:01 +1100

    refactored

--- CoreGameEngine/Shapes/ShapeManager.cs
+++ CoreGameEngine/Shapes/ShapeManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using CoreGameEngine.Extensions;
 using CoreGameEngine.Resources;
 using CoreGameEngine.Structs;
@@ -9,6 +10,7 @@ namespace CoreGameEngine.Shapes
 {
   public class ShapeManager
   {
+    private IDictionary<Point3D, IShape> History { get; }
     private IDictionary<IShape, Point3D> ReferenceLookup { get; }
     public IDictionary<Point3D, IShape> ShapeLocations { get; }
 
@@ -20,6 +22,13 @@ namespace CoreGameEngine.Shapes
 
       ReferenceLookup = referenceLookup ??
                         throw new ArgumentNullException(nameof(referenceLookup), Exceptions.ArgumentIsNull);
+
+      History = new Dictionary<Point3D, IShape>();
+      foreach (var (point, shape) in shapeLocations)
+      {
+        var newShape = shape.CreateNewShapeFrom<IShape>();
+        History.Add(point, newShape);
+      }
     }
 
     public static ShapeManager NewManager()
@@ -27,12 +36,7 @@ namespace CoreGameEngine.Shapes
       return new ShapeManager(new Dictionary<Point3D, IShape>(), new Dictionary<IShape, Point3D>());
     }
 
-    public void CreateNewShape(string shape, char tile, Point3D pos)
-    {
-      Add(Shape.New(shape, tile, pos));
-    }
-
-    public void Add([NotNull] IShape shape)
+    public void Add<T>([NotNull] IShape shape) where T : IShape
     {
       if (shape is null)
       {
@@ -51,6 +55,7 @@ namespace CoreGameEngine.Shapes
 
       ShapeLocations.Add(shape.Position, shape);
       ReferenceLookup.Add(shape, shape.Position);
+      History.Add(shape.Position, shape.CreateNewShapeFrom<T>());
     }
 
     public void Remove(Point3D key)
@@ -62,58 +67,60 @@ namespace CoreGameEngine.Shapes
 
       ReferenceLookup.Remove(ShapeLocations[key]);
       ShapeLocations.Remove(key);
+      History.Remove(key);
     }
 
-    public void UpdateShape([NotNull] IShape newShape, Point3D newPoint)
-    {
-      if (newShape is null)
-      {
-        throw new ArgumentNullException(nameof(newShape), Exceptions.ArgumentIsNull);
-      }
-
-      if (!ReferenceLookup.ContainsKey(newShape))
-      {
-        throw new InvalidOperationException(Exceptions.Dictionary_InvalidKey);
-      }
-
-      ShapeLocations.UpdateKey(newPoint, ReferenceLookup[newShape]);
-      ReferenceLookup[newShape] = newPoint;
-    }
-
-    public void UpdateShape([NotNull] IShape newShape, int newX, int newY, int newZIndex)
-    {
-      var newPoint = new Point3D(newX, newY, newZIndex);
-      UpdateShape(newShape, newPoint);
-    }
-
-    public void UpdateScreen()
-    {
-      foreach (var (shape, point3D) in ReferenceLookup)
-      {
-        if (shape.Updated)
-        {
-          Erase(shape);
-          Draw(shape);
-
-          shape.Updated = false;
-        }
-      }
-    }
-
-    public static void Erase(IShape shape)
+    public void UpdateShape(IShape shape)
     {
       if (shape is null)
       {
         throw new ArgumentNullException(nameof(shape), Exceptions.ArgumentIsNull);
       }
 
-      foreach (var (point, _) in shape.Glyphs)
+      if (!ReferenceLookup.ContainsKey(shape))
       {
-        Console.SetCursorPosition(shape.Position.X + point.X, shape.Position.Y + point.Y);
-        Console.Write(' ');
+        throw new InvalidOperationException(Exceptions.Dictionary_InvalidKey);
       }
 
-      Console.ForegroundColor = ConsoleColor.White;
+      History[ReferenceLookup[shape]] = shape.CreateNewShapeFrom(shape.GetType());
+      ShapeLocations.UpdateKey(ReferenceLookup[shape], shape.Position);
+      ReferenceLookup[ShapeLocations[shape.Position]] = shape.Position;
+
+      shape.Updated = false;
+    }
+
+    public void UpdateScreen()
+    {
+      var shapes = ReferenceLookup
+        .Select(r => r.Value)
+        .OrderBy(v => v.Z);
+
+      foreach (var shape in shapes)
+      {
+        var current = ShapeLocations[shape];
+        if (current.Updated)
+        {
+          Erase(current);
+          Draw(current);
+          UpdateShape(current);
+        }
+      }
+    }
+
+
+    public void Erase(IShape shape)
+    {
+      if (shape is null)
+      {
+        throw new ArgumentNullException(nameof(shape), Exceptions.ArgumentIsNull);
+      }
+
+      var oldShape = History[ReferenceLookup[shape]];
+
+      foreach (var (point, _) in oldShape.Glyphs)
+      {
+        Write(' ', shape.Position.Add(point));
+      }
     }
 
     public static void Draw(IShape shape)
@@ -125,10 +132,15 @@ namespace CoreGameEngine.Shapes
 
       foreach (var (point, glyph) in shape.Glyphs)
       {
-        Console.SetCursorPosition(shape.Position.X + point.X, shape.Position.Y + point.Y);
-        Console.ForegroundColor = glyph.Color;
-        Console.Write(glyph.Character);
+        Write(glyph.Character, shape.Position.Add(point), glyph.Color);
       }
+    }
+
+    private static void Write(char character, Point3D position, ConsoleColor color = ConsoleColor.White)
+    {
+      Console.SetCursorPosition(position.X, position.Y);
+      Console.ForegroundColor = color;
+      Console.Write(character);
 
       Console.ForegroundColor = ConsoleColor.White;
     }
